1de760ab4213415070e5a136c489636a684c7d5e
roy-Pritom <pritombd.hitcse2020@gmail.com>
2024-11-04 09:47:29 +0600

    Change api url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.API_URL,
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
   }),
   endpoints: () => ({}),
   tagTypes: TagTypeList,
