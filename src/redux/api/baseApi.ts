44648da854df7c5ff58aa0b07fbdcaef1b5415e2
roy-Pritom <pritombd.hitcse2020@gmail.com>
2024-11-04 09:15:10 +0600

    URL Set

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://192.168.0.106:5000/api/v1",
+    baseUrl: process.env.API_URL,
   }),
   endpoints: () => ({}),
   tagTypes: TagTypeList,
